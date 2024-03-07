3dfa3a9ef138df02f1ef907d78bfc92ae91e9b18
Gabriel <gabrielhenriquedps@gmail.com>
2024-03-07 01:16:33 -0300

    feat: add create sale route

--- src/main/java/com/gabrielhenrique/salesapicomeia/modules/sales/controllers/SalesController.java
+++ src/main/java/com/gabrielhenrique/salesapicomeia/modules/sales/controllers/SalesController.java
@@ -2,18 +2,24 @@ package com.gabrielhenrique.salesapicomeia.modules.sales.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gabrielhenrique.salesapicomeia.modules.sales.SalesEntity;
+
 @RestController
-@RequestMapping("/salesController")
+@RequestMapping("/sale")
 public class SalesController {
+
     @GetMapping("/getSales")
     public String getSales() {
         return "Primeira Rota";
     }
-    @PostMapping("/getSales")
-    public String postSales() {
-        return "Segunda Rota";
+
+    @PostMapping("/create")
+    public void create(@RequestBody SalesEntity salesEntity){
+        System.out.println("Venda");
+        System.out.println(salesEntity);
     }
 }
