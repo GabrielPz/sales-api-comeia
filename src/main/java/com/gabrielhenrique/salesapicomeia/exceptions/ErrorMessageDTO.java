9282f52090ccf0d4a6ea893794d733a2cd77bb6c
Gabriel <gabrielhenriquedps@gmail.com>
2024-03-07 01:58:51 -0300

    feat: add validation to sales entity

---
+++ src/main/java/com/gabrielhenrique/salesapicomeia/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,11 @@
+package com.gabrielhenrique.salesapicomeia.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
--- src/main/java/com/gabrielhenrique/salesapicomeia/modules/sales/SalesEntity.java
+++ src/main/java/com/gabrielhenrique/salesapicomeia/modules/sales/SalesEntity.java
@@ -7,17 +7,28 @@ import java.util.UUID;
 
 import org.hibernate.validator.constraints.Length;
 
-import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotNull;
+
+// import jakarta.validation.constraints.Size;
+// import jakarta.validation.constraints.Email;
 
 @Data
 public class SalesEntity {
     private UUID id;
 
-    @Length(min = 10, max = 200)
+    @NotNull(message = "O campo id não pode ser nulo.")
+    @Length(min = 10, max = 200, message = "A descrição precis conter entre 10 e 200 carcateres")
     private String saleDescription;
+
+    @NotNull(message = "O campo id não pode ser nulo.")
     private String itemsSold;
+
+    @NotNull(message = "O campo id não pode ser nulo.")
     private Integer itemQuantity;
+
+    @NotNull(message = "O campo id não pode ser nulo.")
     private Double itemPrice;
+
+    @NotNull(message = "O campo id não pode ser nulo.")
     private Date saleDate;
 }
