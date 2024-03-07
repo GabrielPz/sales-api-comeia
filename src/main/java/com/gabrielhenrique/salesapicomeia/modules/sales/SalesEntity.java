183f239b6fd6e061253028b8ffa4c6fedec1c28b
Gabriel <gabrielhenriquedps@gmail.com>
2024-03-07 01:09:40 -0300

    feat: add saleEntity

--- src/main/java/com/gabrielhenrique/salesapicomeia/modules/sales/SalesEntity.java
+++ src/main/java/com/gabrielhenrique/salesapicomeia/modules/sales/SalesEntity.java
@@ -1,5 +1,16 @@
 package com.gabrielhenrique.salesapicomeia.modules.sales;
 
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
 public class SalesEntity {
-    
+    private UUID id;
+    private String saleDescription;
+    private String itemsSold;
+    private Integer itemQuantity;
+    private Double itemPrice;
+    private Date saleTime;
 }
