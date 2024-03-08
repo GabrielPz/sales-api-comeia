a4b6bcae628e76936458dd3b7975c538e9f7a6a7
Gabriel <gabrielhenriquedps@gmail.com>
2024-03-08 10:29:01 -0300

    feat: add swagger documentation

--- src/main/java/com/gabrielhenrique/salesapicomeia/SalesApiComeiaApplication.java
+++ src/main/java/com/gabrielhenrique/salesapicomeia/SalesApiComeiaApplication.java
@@ -4,8 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.gabrielhenrique")
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Vendas Comeia",
+		description = "Api de vendas para o desafio Java Pleno Comeia 2024",
+		version = "1.0"
+	)
+)
 public class SalesApiComeiaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/gabrielhenrique/salesapicomeia/SecurityConfig.java
+++ src/main/java/com/gabrielhenrique/salesapicomeia/SecurityConfig.java
@@ -11,10 +11,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private static final String[] SWAGGER_LIST = {
+        "/swagger-ui/**",
+        "/v3/api-docs/**",
+        "swagger-resources/**"
+    };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf().disable().authorizeHttpRequests().anyRequest().authenticated();
+        http.csrf().disable().authorizeHttpRequests((authz) -> authz
+        .requestMatchers(SWAGGER_LIST).permitAll() // Permite acesso aos endpoints do Swagger sem autenticação.
+        .anyRequest().authenticated() // Todos os outros requests precisam de autenticação.
+    );
         http.oauth2ResourceServer().jwt();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
